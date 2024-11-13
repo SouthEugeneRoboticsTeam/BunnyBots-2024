76de75b56bcb7c1780748242de147f6040589983
Kona-Watson <watsonkona405@gmail.com>
2024-11-13 09:41:01 -0800

    Renamed some things to Channel, tried to get PWM to work? ( ＾◡＾)っ ♡

--- src/main/java/org/sert2521/bunnybots2024/Constants.kt
+++ src/main/java/org/sert2521/bunnybots2024/Constants.kt
@@ -9,8 +9,8 @@ object PhysicalConstants {
 }
 
 object ElectricIDs{
-    const val FAN_PWM_ID = -1
-    const val FAN_SERVO_ID = -1
+    const val FAN_PWM_CHANNEL = -1
+    const val FAN_SERVO_CHANNEL = -1
 }
 
 object TunedConstants{
--- src/main/java/org/sert2521/bunnybots2024/subsystems/Fan.kt
+++ src/main/java/org/sert2521/bunnybots2024/subsystems/Fan.kt
@@ -1,12 +1,15 @@
 package org.sert2521.bunnybots2024.subsystems
 
+import edu.wpi.first.wpilibj.PWM
+import edu.wpi.first.wpilibj.Servo
 import edu.wpi.first.wpilibj2.command.Subsystem
 import org.sert2521.bunnybots2024.ElectricIDs
 
 
 object Fan : Subsystem {
 
-    //private val fanServo = Servo(ElectricIDs.FAN_SERVO_ID)
+    private val fan = PWM(ElectricIDs.FAN_PWM_CHANNEL)
+    private val fanServo = Servo(ElectricIDs.FAN_SERVO_CHANNEL)
 
     init {
 
