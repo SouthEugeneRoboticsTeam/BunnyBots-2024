721134a3bb159d24ff4767d529720f62770c6c99
Benji <benji.urbancic@gmail.com>
2024-11-14 16:00:48 -0800

    Not First commit

--- src/main/java/org/sert2521/bunnybots2024/Input.kt
+++ src/main/java/org/sert2521/bunnybots2024/Input.kt
@@ -1,4 +1,9 @@
 package org.sert2521.bunnybots2024
 
+import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.XboxController
+
 object Input {
+    val driverController = XboxController(0)
+    val gunnerController = Joystick(1)
 }
