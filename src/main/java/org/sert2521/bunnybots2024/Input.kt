96a905887e7d6848dad7eb173031e46d1a93c937
Drive 1/Theodore <benji.urbancic@gmail.com>
2024-11-24 13:08:14 -0800

    added example joystick button

--- src/main/java/org/sert2521/bunnybots2024/Input.kt
+++ src/main/java/org/sert2521/bunnybots2024/Input.kt
@@ -2,8 +2,13 @@ package org.sert2521.bunnybots2024
 
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj2.command.button.JoystickButton
 
 object Input {
     val driverController = XboxController(0)
     val gunnerController = Joystick(1)
+
+    init{
+        val exampleButton = JoystickButton(gunnerController, 1)
+    }
 }
