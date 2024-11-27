1abaf44fb03e56f4f3596cea784126e6f9300ebc
Ely <elystanmay@gmail.com>
2024-11-26 17:48:20 -0800

    Removed second beambreak const

--- src/main/java/org/sert2521/bunnybots2024/Constants.kt
+++ src/main/java/org/sert2521/bunnybots2024/Constants.kt
@@ -15,7 +15,6 @@ object ElectricIDs{
     const val INTAKE_ID = -1
     const val INDEXER_BEAM_BREAK_ID = -1
     const val INDEXER_MOTOR_ID = -1
-    const val INDEXER_BEAM_BREAK_ID = -1
 }
 
 object TunedConstants{
