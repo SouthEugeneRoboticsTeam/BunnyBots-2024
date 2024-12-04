016c7ed8fd731bb7b9f8783f9d6c5355d71f3c2a
Nathan <ndgunder07@gmail.com>
2024-12-03 19:36:15 -0800

    Outtake Subsystem Start

--- src/main/java/org/sert2521/bunnybots2024/Constants.kt
+++ src/main/java/org/sert2521/bunnybots2024/Constants.kt
@@ -19,5 +19,13 @@ object TunedConstants{
     const val WRIST_G = 0.0
     const val WRIST_V = 0.0
     const val WRIST_A = 0.0
+    const val WRIST_P = 0.0
+    const val WRIST_I = 0.0
+    const val WRIST_D = 0.0
+    const val WRIST_ANGLE_TOLERANCE = 0.1
+}
+
+object RuntimeConstants {
+    var wristSetPoint = 0.0
 }
 
--- src/main/java/org/sert2521/bunnybots2024/commands/SetWristCommand.kt
+++ src/main/java/org/sert2521/bunnybots2024/commands/SetWristCommand.kt
@@ -1,27 +1,51 @@
 package org.sert2521.bunnybots2024.commands
 
 import edu.wpi.first.math.controller.ArmFeedforward
+import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.wpilibj2.command.Command
+import org.sert2521.bunnybots2024.RuntimeConstants
 import org.sert2521.bunnybots2024.TunedConstants
 import org.sert2521.bunnybots2024.subsystems.WristSubsystem
+import kotlin.math.PI
 
-class SetWristCommand : Command() {
+class SetWristCommand(private val goal: Double) : Command() {
     private var wristAngle = WristSubsystem.getRadians()
     private var feedForward = ArmFeedforward(TunedConstants.WRIST_S, TunedConstants.WRIST_G, TunedConstants.WRIST_V, TunedConstants.WRIST_A)
+    private var pid = PIDController(TunedConstants.WRIST_P, TunedConstants.WRIST_I, TunedConstants.WRIST_D)
+
+    private var done = false
 
     init {
         // each subsystem used by the command must be passed into the addRequirements() method
         addRequirements(WristSubsystem)
     }
 
-    override fun initialize() {}
+    override fun initialize() {
+        wristAngle = WristSubsystem.getRadians()
+        pid.reset()
+        RuntimeConstants.wristSetPoint=goal
+    }
 
-    override fun execute() {}
+    override fun execute() {
+        done = false
+        wristAngle = WristSubsystem.getRadians()
+        val pidResult = pid.calculate(wristAngle+2*PI, goal+2*PI)
+        val feedforwardResult = feedForward.calculate(wristAngle, pid.setpoint)
+        WristSubsystem.setVoltage(feedforwardResult + pidResult)
+
+        if(WristSubsystem.getRadians() > goal-TunedConstants.WRIST_ANGLE_TOLERANCE && WristSubsystem.getRadians() > goal + TunedConstants.WRIST_ANGLE_TOLERANCE) {
+            done = true
+        }
+    }
 
     override fun isFinished(): Boolean {
         // TODO: Make this return true when this Command no longer needs to run execute()
-        return false
+        return done
     }
 
-    override fun end(interrupted: Boolean) {}
+    override fun end(interrupted: Boolean) {
+        if (interrupted) {
+            RuntimeConstants.wristSetPoint
+        }
+    }
 }
